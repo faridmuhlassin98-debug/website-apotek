ec161537f8d20842cf43d8fb0a9433d0adacd824
farid_muhl <farid.muhlassin98@gmail.com>
2025-12-06 20:17:16 +0700

    inisialisasi database ke page daftar obat

--- apotek_mini/index.php
+++ apotek_mini/index.php
@@ -1,36 +1,39 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>MEDIVO STORE</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<div class="sidebar">
-    <h2>MEDIVO STORE</h2>
-    <a href="index.php">Beranda</a>
-    <a href="obat.php">Daftar Obat</a>
-</div>
-
-<div class="content">
-    <h1>Hello!! Selamat Datang di Medivo Store</h1>
-    <p>Ini adalah sistem pengelolaan apotik sederhana berbasis web.</p>
-    <p>Dibuat menggunakan PHP dan MySQL.</p>
-
-    <div class="box">
-        <h3>🔹 Fitur</h3>
-        <ul>
-            <li>Kelola data obat</li>
-            <li>Tampilkan gambar dan deskripsi obat</li>
-            <li>Terkoneksi database</li>
-        </ul>
+    <div class="sidebar">
+        <h2>APOTEK SEHAT</h2>
+        <a href="index.php">Beranda</a>
+        <a href="obat.php">Daftar Obat</a>
     </div>
 
-    <div class="box">
-        <h3>🔹 Petunjuk</h3>
-        <p>Gunakan menu di samping untuk melihat daftar buku.</p>
+    <div class="content">
+        <h1>Hello!! Selamat Datang di Medivo Store</h1>
+        <p>Ini adalah sistem pengelolaan apotik sederhana berbasis web.</p>
+        <p>Dibuat menggunakan PHP dan MySQL.</p>
+
+        <div class="box">
+            <h3>🔹 Fitur</h3>
+            <ul>
+                <li>Kelola data obat</li>
+                <li>Tampilkan gambar dan deskripsi obat</li>
+                <li>Terkoneksi database</li>
+            </ul>
+        </div>
+
+        <div class="box">
+            <h3>🔹 Petunjuk</h3>
+            <p>Gunakan menu di samping untuk melihat daftar buku.</p>
+        </div>
     </div>
-</div>
 
 </body>
+
 </html>
--- apotek_mini/obat.php
+++ apotek_mini/obat.php
@@ -12,7 +12,7 @@
 <body>
 
     <div class="sidebar">
-        <h2>APOTEK MINIMALIS</h2>
+        <h2>APOTEK SEHAT</h2>
         <a href="index.php">Beranda</a>
         <a href="obat.php">Daftar Obat</a>
     </div>
@@ -24,13 +24,14 @@
         $query = mysqli_query($koneksi, "SELECT * FROM obat ORDER BY nama_obat ASC");
         while ($data = mysqli_fetch_array($query)) {
             ?>
+
             <div class="card">
-                <img src="assets/<?php echo $data['gambar']; ?>" alt="<?php echo $data['nama_obat']; ?>">
-                <h3><?php echo $data['nama_obat']; ?></h3>
+                <img src="assets/<?php echo $data['gambar']; ?>" alt="<?php echo $data['nama_obat']; ?>" class="card-img">
+                <h3 class="card-title"><?php echo $data['nama_obat']; ?></h3>
                 <p><?php echo substr($data['deskripsi'], 0, 100); ?></p>
                 <p class="harga">Rp <?php echo number_format($data['harga'], 0, ',', '.'); ?></p>
 
-                <a class="btn" href="beli.php?id=<?php echo $data['id']; ?>">BELI</a>
+                <a class="btn-beli" href="beli.php?id=<?php echo $data['id']; ?>">BELI</a>
             </div>
         <?php } ?>
     </div>
