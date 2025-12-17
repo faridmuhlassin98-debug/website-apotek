d5992407fe2ecd02a05cd89c1dd9102423f32a2c
Gendis Rahmayati <gendisrahmayati@gmail.com>
2025-12-17 06:53:39 -0800

    updateee

--- apotek mhs 0/beranda.php
+++ apotek mhs 0/beranda.php
@@ -15,7 +15,7 @@ $sql = "SELECT * FROM golongan_obat ORDER BY id";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda - Apotek Mahasiswa</title>
+    <title>Beranda - MEDIFO</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
